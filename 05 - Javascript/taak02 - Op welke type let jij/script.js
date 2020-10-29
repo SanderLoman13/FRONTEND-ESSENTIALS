0ddd0af5e3babb448decfd8e6c01cc6a9ba24794
Sander Loman <sander.loman@student.rocva.nl>
2020-10-29 11:44:32 +0100

    datatypes af

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,15 @@
+let mijnVoornaam = "Sander"
+let mijnAchternaam = "Loman"
+let leeftijd = "17"
+let ikBenStemGerechtig = "false"
+let ikMagLegaalAlcoholKopen = "false"
+let ikMagBrommerRijden = "false"
+let SoftwareOntwikkeling = "true"
+console.log(mijnVoornaam);
+console.log(mijnAchternaam);
+console.log(leeftijd);
+console.log(ikBenStemGerechtig);
+console.log(ikMagLegaalAlcoholKopen);
+console.log(ikMagBrommerRijden);
+console.log(SoftwareOntwikkeling);
+
