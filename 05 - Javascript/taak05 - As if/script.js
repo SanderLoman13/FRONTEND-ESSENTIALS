149c0f754bc1be2bf54dbd5ccfefb0b8df89ae95
Sander Loman <sander.loman@student.rocva.nl>
2020-10-30 11:48:56 +0100

    As If afgemaakt

---
+++ 05 - Javascript/taak05 - As if/script.js
@@ -0,0 +1,10 @@
+let mijnLeeftijd = 17;
+let stemGerechtigeleeftijd = 18;
+
+if (mijnLeeftijd < stemGerechtigeleeftijd){
+    alert("Je mag nog niet stemmen.");
+}
+
+if (mijnLeeftijd >= stemGerechtigeleeftijd){
+    alert("Jij bent " + mijnLeeftijd + " je mag stemmen met de verkiezingen")
+}
