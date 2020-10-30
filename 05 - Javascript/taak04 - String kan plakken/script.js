4136c297f887c992bad71bb8bf14aa0943d01510
Sander Loman <sander.loman@student.rocva.nl>
2020-10-30 11:11:39 +0100

    Sting kan plakken af

---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,7 @@
+let mijnVoornaam = "Sander "
+let mijnAchternaam = "Loman "
+let mijnLeeftijd = "17"
+let volledigeNaam = mijnVoornaam + mijnAchternaam + mijnLeeftijd;
+let volledigeZin = "Mijn naam is " + mijnVoornaam + mijnAchternaam + "en mijn leeftijd is " + mijnLeeftijd + " jaar";
+console.log(volledigeNaam);
+console.log(volledigeZin);
